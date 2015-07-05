5ccb70b10b963ce2fc09ed6a56eb3ac4ec2af66b
Pavel Apostolov <pavel.apostolov@yahoo.com>
2015-07-05 13:35:35 +0300

    first

---
+++ 01_Intro/01_Intro/HelloCsharp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class HelloCsharp
+{
+    static void Main()
+    {
+        string greeting = "Hello, C#!";
+        Console.WriteLine(greeting);
+    }
+}
---
+++ 01_Intro/PrintMyName/PrintMyName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class PrintMyName
+{
+    static void Main()
+    {
+        string name = "Pavel Apostolov";
+        Console.WriteLine(name);
+        string asd = "asd";
+        Console.WriteLine(asd);
+        asd = "asdasda";
+        Console.WriteLine(asd);
+    }
+}
