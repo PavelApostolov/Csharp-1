b0512defe3e689634e0a2b05d95cbede30eeafff
Pavel Apostolov <pavel.apostolov@yahoo.com>
2016-04-25 12:44:17 +0300

    Zadachi

---
+++ Zadachi/05.SubsetSum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.SubsetSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long s = long.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            long[] nums = new long[n];
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = long.Parse(Console.ReadLine());
+            }
+            int maxI = 1;
+            for (int i = 0; i < n; i++)
+			{
+                maxI *= 2;
+			}
+            maxI -= 1;
+            //equals maxI = (int)Math.Pow((double)2,n) -1;
+
+            int count = 0;
+            for (int i = 1; i <= maxI; i++)
+            {
+                long currentSum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    int mask = 1 << j;
+                    int nMask = i & mask;
+                    int bit = nMask >> j;
+                    if (bit==1)
+                    {
+                        currentSum += nums[j];
+                    }
+                    if (currentSum == s)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
