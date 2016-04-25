88a5970139c9473a0bf844689ab85b08361509cb
PavelApostolov <pavel.apostolov@yahoo.com>
2016-04-25 11:29:52 +0300

    Update Program.cs

--- Zadachi/02.Astrological digits/Program.cs
+++ Zadachi/02.Astrological digits/Program.cs
@@ -37,6 +37,30 @@ namespace Problem_2_Astrological_Digits
             }
 
             Console.WriteLine(sumOfDigits);
+        
+                        /*string inputNumber = Console.ReadLine();
+            int sum = 0;
+
+            while (true)
+            {
+                for (int i = 0; i < inputNumber.Length; i++)
+                {
+                    if (!(inputNumber[i] == '.' || inputNumber[i] == '-'))
+                    {
+                        sum += int.Parse(inputNumber[i].ToString());
+                    }
+                }
+                if (sum < 10)
+                {
+                    break;
+                }
+
+                inputNumber = sum.ToString();
+                sum = 0;
+            }
+
+            Console.WriteLine(sum);
+            */
         }
     }
 }
